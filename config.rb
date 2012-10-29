162a606ad71fbe4e1747b2162678e026e70207e4
Anahkiasen <ehtnam6@gmail.com>
2012-10-29 15:46:13 +0100

    Add compass configuration file

---
+++ config.rb
@@ -0,0 +1,21 @@
+# Folders
+css_dir          = "css"
+fonts_dir        = "fonts"
+images_dir       = "img"
+javascripts_dir  = "js"
+
+# Options
+line_comments    = false
+output_style     = :expanded
+preferred_syntax = :sass
+relative_assets  = true
+
+# Extensions
+require 'animate'
+require 'modular-scale'
+require 'normalize'
+require 'rgbapng'
+require 'susy'
+
+# Disable cache busting
+# asset_cache_buster :none
